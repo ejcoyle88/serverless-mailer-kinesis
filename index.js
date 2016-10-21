b64585b5714041033d551973426fdb829e2ba92f
Elliot Coyle <elliot@vaetek.biz>
2016-10-21 14:32:10 +0000

    .

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ const Mailer = require('./lib');
 
 module.exports.send = (event, context, callback) => {
   const respCallback = callback.bind(this, null);
-  const mailer = new Mailer(event);
-  return BbPromise.resolve()
-    .bind(mailer)
-    .then(mailer.validate)
-    .then(mailer.render)
-    .then(mailer.send)
-    .then(respCallback)
-    .catch(callback);
+  event.Records.forEach((r) => {
+    var buffer = new Buffer(record.kinesis.data, 'base64').toString('ascii');
+    var payload = JSON.parse(buffer);
+    const mailer = new Mailer(payload);
+    return BbPromise.resolve()
+      .bind(mailer)
+      .then(mailer.validate)
+      .then(mailer.render)
+      .then(mailer.send)
+      .then(respCallback)
+      .catch(callback);
+  });
 };
