a2c083c604242a1a0b3ce336ac0b5f85fe60bbf1
Elliot Coyle <elliot@vaetek.biz>
2016-10-21 13:56:52 +0000

    Half way through some changes, just wanting to setup github

--- index.js
+++ index.js
@@ -6,16 +6,13 @@ const BbPromise = require('bluebird');
 const Mailer = require('./lib');
 
 module.exports.send = (event, context, callback) => {
+  const respCallback = callback.bind(this, null);
   const mailer = new Mailer(event);
   return BbPromise.resolve()
     .bind(mailer)
     .then(mailer.validate)
     .then(mailer.render)
     .then(mailer.send)
-    .then(response => {
-      callback(null, response);
-    })
-    .catch(e => {
-      callback(e);
-    });
+    .then(respCallback)
+    .catch(callback);
 };
